a127bf63793d28b91138d0afb059834457f0382d
amitsgh <amitsingh1911nov@gmail.com>
2023-10-20 21:41:45 +0530

    Setup routes

--- server/routes/client.js
+++ server/routes/client.js
@@ -1,5 +1,17 @@
-import express from "express";
+import express from 'express';
+import {
+    getCustomers,
+    getGeoData,
+    getGeography,
+    getProducts,
+    getTransactions,
+} from '../controllers/client.js';
 
 const router = express.Router();
+router.get('/products', getProducts);
+router.get('/customers', getCustomers);
+router.get('/transactions', getTransactions);
+router.get('/geography', getGeography);
+router.get('/geo-data', getGeoData);
 
 export default router;
